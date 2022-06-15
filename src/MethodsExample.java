4f4507f304f4ec181eee24310353366643008567
mpereira0726 <106694216+mpereira0726@users.noreply.github.com>
2022-06-15 16:10:03 -0400

    MethodsExample

---
+++ src/MethodsExample.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class MethodsExample {
+    public static void main(String[] args){
+    Scanner kdb = new Scanner(System.in);
+    int n1, n2;
+    double average;
+        System.out.print("Enter integer: ");
+        n1 = kdb.nextInt();
+        System.out.print("Enter integer: ");
+        n2 = kdb.nextInt();
+
+    }
+}
